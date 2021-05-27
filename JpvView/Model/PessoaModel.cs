b4676140bef3b7d8b6aec3bbce1ddcdbdf4c86a6
Fabio Lucio Ribeiro <fabioribeiro1904@outlook.com>
2021-05-27 19:55:39 -0300

    Add project files.

---
+++ JpvView/Model/PessoaModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JpvView.Model
+{
+    public class PessoaModel
+    {
+        public Guid Id { get; set; }
+        public string Nome { get;  set; }
+
+        public DateTime Nascimento { get;  set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:c}")]
+        public double Renda { get;  set; }
+
+        public string Cpf { get;  set; }
+    }
+}
